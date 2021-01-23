1e5c9b5198b9145407486649b9cf6e62bc9e6599
ymcastellar <yoycas@hotmail.com>
2021-01-23 16:50:44 -0500

    Vue files added

---
+++ twitter-tutorial/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    @{{user.username}} - {{fullName}}
+    <strong>Followers:</strong> {{followers}}
+    <button v-on:click="followUser">Follow</button>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "yoycas",
+        firstName: "Yoyman",
+        lastName: "Castellar",
+        email: "yoycas@hotmail.com",
+        isAdmin: true
+      }
+    }
+  },
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if(oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follwer!`)
+      }
+    }
+  },
+
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+
+  methods: {
+    followUser () {
+      this.followers++
+    }
+  },
+
+  mounted() {
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+}
+</style>
